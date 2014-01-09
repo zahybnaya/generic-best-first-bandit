33289a1257e4e49699d693da230d21113b6bd47a
Zahy Bnaya <zahy.bnaya@gmail.com>
2014-01-09 03:22:37 +0200

    added documentation to distribution script

--- distribute_exections.sh
+++ distribute_exections.sh
@@ -1,5 +1,23 @@
 #!/bin/bash 
 # get hosts
+                                                                                                    
+####################################################################################################
+# Distributes a task on multiple computers.                                                        #
+# Uses Python                                                                                      #
+#                                                                                                  #
+# 1) Produce an execution file (executions are unique)                                             #
+# 2) call the script with <working dir> python distributer.py <execution_file>                     #
+# 3) locks are stored at .locks by default                                                         #
+# 4) Assumes a list of hosts at "readyServers"                                                     #
+#                                                                                                  #
+#  Example:                                                                                        #
+#                                                                                                  #
+# distribute_exections.sh "./test_execution/generic-best-first-bandit" "python distributer.py exe" #
+#                                                                                                  #
+####################################################################################################
+
+
+
 hosts=(`cat readyServers | uniq`)
 WORKING_DIR="$1"
 EXPERIMENTER_COMMAND="$2"
