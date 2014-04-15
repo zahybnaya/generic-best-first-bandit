bc4beb2812991efb24acd12440ba31a559a24805
AlonPalombo <alonpal@post.bgu.ac.il>
2014-04-15 11:46:46 -0700

    mancala experiments with MMUCT for different ci thresholds

--- mancala_raw_data/summarize_csv.sh
+++ mancala_raw_data/summarize_csv.sh
@@ -5,7 +5,7 @@
 #
 cd ./mancala_parentCI
 RESULT_FILE='../mancala_data_parentCI.csv'
-echo "pits,backop,heuristics,c,iterations,seed,num_games,domain,max_alg,min_alg,num_of_games,num_of_draws,num_of_incomplete,max_win,min_win,total_win_games,max_time,min_time,avg_score" > ${RESULT_FILE}
+echo "pits,backop,heuristics,c,iterations,seed,num_games,ci_threshold,domain,max_alg,min_alg,num_of_games,num_of_draws,num_of_incomplete,max_win,min_win,total_win_games,max_time,min_time,avg_score" > ${RESULT_FILE}
 grep '^[0-9],' * | sed 's/_/,/g' | sed 's/:/,/g' | awk ' BEGIN {FS=","} {$1=10; print $0}' | sed 's/ /,/g' | sed 's/.csv//g' >> ${RESULT_FILE}
 
 FIELDS=`cat ${RESULT_FILE} | awk 'BEGIN {FS=","} {print NF}' | sort | uniq | wc -l`
