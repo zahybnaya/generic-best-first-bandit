4f51f61f66ea0a1846b331c837eada9db808c68f
AlonPalombo <alonpal@post.bgu.ac.il>
2014-05-04 01:49:04 -0700

    evalstates uct tree building experiments

--- run_experiments/eval_states/calc.R
+++ run_experiments/eval_states/calc.R
@@ -1,7 +1,7 @@
 #R script for generiting data
 # make sure there are no repeating head lines in states.csv
 
-d=read.csv('states.csv')
+d=read.csv('states_ci_build_1000games.csv', stringsAsFactors=FALSE)
 
 splitByH=split(d,d$H)
 for(h in names(splitByH)){
@@ -11,15 +11,17 @@ for(h in names(splitByH)){
 	for(ci in names(splitByC)){
 		d<-splitByC[[ci]]
 		cat("\nCI_Threshold:",ci,"\n")
-		splitByIt=split(d,d$iterations)
+		splitByIt=split(d,d$Iterations)
 		for(it in names(splitByIt)){
 			d<-splitByIt[[it]]
 			cat("\t#Iterations:",it,"\n")
-			cat("\tMMF:",nrow(d[d$GS_Move==d$MMF_Move,])/nrow(d),"\n")
-			cat("\tCI:",nrow(d[d$GS_Move==d$CI_Move,])/nrow(d),"\n")
+			cat("\tMMB:",nrow(d[d$GS_Move==d$MMB_Move,])/nrow(d),"\n")
+			cat("\tCIB:",nrow(d[d$GS_Move==d$CIB_Move,])/nrow(d),"\n")
 			cat("\tUCT:",nrow(d[d$GS_Move==d$UCT_Move,])/nrow(d),"\n")
-			cat("\tDifference from UCT:",nrow(d[d$GS_Move!=d$UCT_Move & d$CI_Move==d$GS_Move ,]),"\n")
-			cat("\tDifference from MMF:",nrow(d[d$GS_Move!=d$MMF_Move & d$CI_Move==d$GS_Move ,]),"\n")
+			#cat("\tDifference from UCT:",nrow(d[d$GS_Move!=d$UCT_Move & d$CIB_Move==d$GS_Move ,]),"\n")
+			#cat("\tDifference from MMB:",nrow(d[d$GS_Move!=d$MMB_Move & d$CIB_Move==d$GS_Move ,]),"\n")
+			cat("\tDifference from UCT:",nrow(d[d$CIB_Move!=d$UCT_Move ,]),"\n")
+			cat("\tDifference from MMB:",nrow(d[d$CIB_Move!=d$MMB_Move ,]),"\n")
 		}
 	}
 }
