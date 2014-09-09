6894abc7282a08660dad609ed0918ca39b1a3835
Zahy Bnaya <zahy@ubuntu3.cs.bgu.ac.il>
2014-09-10 00:33:44 +0300

    added seg-results

---
+++ experiments_seg/summarize_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#
+#  Aggregate all csv's to files
+#
+#
+RESULT_FILE='../seg_mancala_data.csv'
+echo "iterations,a1,a2,seg,max_alg, min_alg, num_of_games,num_of_draws,num_of_incomplete,max_win,min_win,total_win_games,max_time,min_time,score" > ${RESULT_FILE}
+grep '^[0-9],' *.csv | sed 's/K/000,/g' | sed 's/VS/,/g' | sed 's/B/,/g' | sed 's/:/,/g' | sed 's/ /,/g' | sed 's/.csv//g' >> ${RESULT_FILE}
+
+
+
+
+
+FIELDS=`cat ${RESULT_FILE} | awk 'BEGIN {FS=","} {print NF}' | sort | uniq | wc -l`
+if (( ${FIELDS} > 1)); then 
+	echo "ERROR! Problem genereting summary file, lines have different fields. some data is missing. Please check the file manually!"
+fi
+
+
