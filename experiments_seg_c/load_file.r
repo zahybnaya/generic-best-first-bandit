50e284af3e5de223e7e3a5ae50c45668a1a5725c
Zahy Bnaya <zahy.bnaya@gmail.com>
2014-09-13 12:59:06 +0300

    plot

--- experiments_seg_c/load_file.r
+++ experiments_seg_c/load_file.r
@@ -16,10 +16,11 @@ if(nrow(vmcdp)>0){
 print("seg1 length:")
 print(nrow(v))
 pdf('mc_1.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype=factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2))
-      + scale_shape_manual(values=c(6,5,1))
-      )
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
+
+
 
 # Display for seg=4 and against MC 
 v=data[data$seg==4 & data$a2 == "MC",]
@@ -35,7 +36,8 @@ if(nrow(vmcdp)>0){
 print("seg4 length")
 print(nrow(v))
 pdf('mc_4.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=4") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 # Display for seg=8 and against MC 
@@ -52,7 +54,8 @@ if(nrow(vmcdp)>0){
 print("seg8 length")
 print(nrow(v))
 pdf('mc_8.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=8") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 
@@ -70,7 +73,8 @@ if(nrow(vmcdp)>0){
 print("seg16 length")
 print(nrow(v))
 pdf('mc_16.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=16") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 # Display for seg=8 and against MC 
@@ -87,7 +91,8 @@ if(nrow(vmcdp)>0){
 print("seg24 length")
 print(nrow(v))
 pdf('mc_24.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=24") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 
@@ -98,7 +103,8 @@ v=data[data$seg==5 & data$a2 == "MC",]
 print("seg5 length:")
 print(nrow(v))
 pdf('mc_5.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=5") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 
@@ -107,7 +113,8 @@ v=data[data$seg==10 & data$a2 == "MC",]
 print("seg10 length:")
 print(nrow(v))
 pdf('mc_10.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=1,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=10") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 
@@ -115,7 +122,8 @@ dev.off()
 v=data[data$seg==20 & data$a2 == "MC",]
 print(nrow(v))
 pdf('mc_20.pdf')
-print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=factor(v$a1))) + geom_line(size=2,aes(linetype = factor(v$a1))) + geom_point(size=4, shape=factor(v$a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=20") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.15,0.85), legend.key.size= unit(1.9,"lines")) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)))
+print(ggplot(v,aes(x=v$iterations, y=v$max_win, group=a1)) + geom_line(size=1,aes(linetype=a1)) + geom_point(size=4,aes(shape=a1)) +  ylab(" % win rate") + ylim(0,0.9)+ xlab("#iterations (thousands)") + scale_linetype_discrete(name="Seg=1") + theme_bw(base_size = 32, base_family = "Helvetica")+ theme(legend.position = c(0.13,0.88), legend.key.size= unit(1.9,"lines"), legend.title=element_blank()) + geom_hline(yintercept=0.5, linecolor=5, linewidth=1) + scale_x_continuous(labels= function(x) format(x/1000,digits = 2)) + scale_linetype_manual(values=c("solid","dotdash", "dotted"))
++ scale_shape_manual(values=c(8,15,6)))
 dev.off()
 
 
