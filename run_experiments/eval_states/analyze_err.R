1ffe36127ff163316948ca1e4468a9862bacd20c
Zahy Bnaya <zahy.bnaya@gmail.com>
2014-10-02 16:55:24 +0300

    shortened tvalues and analyze err

---
+++ run_experiments/eval_states/analyze_err.R
@@ -0,0 +1,11 @@
+
+
+data=read.csv(file='../../results/evalstates/states.csv',sep=',')
+
+data$ucterror<-abs(data$UCT_Move_Val-data$GS_Move_Val)
+data$cdperror<-abs(data$CDP_Move_Val-data$GS_Move_Val)
+data$mmerror<-abs(data$MM_Move_Val-data$GS_Move_Val)
+
+print(paste("UCT_ERROR:",mean(data$ucterror)))
+print(paste("CDP_ERROR:",mean(data$cdperror)))
+print(paste("MM_ERROR:",mean(data$mmerror)))
